0bd2db7d61701c439c48862523efba4712a3f6a6
Tom McLean <mcleantom97@gmail.com>
2022-08-29 10:38:51 +0100

    tmp

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -4,6 +4,26 @@
 #include <sys/wait.h>
 #include <string>
 #include <linenoise.h>
+#include <vector>
+#include <sstream>
+
+std::vector<std::string> split(const std::string &s, char delimeter) {
+    std::vector<std::string> out{};
+    std::stringstream ss {s};
+    std::string item;
+
+    while (std::getline(ss, item, delimeter)) {
+        out.push_back(item);
+    }
+
+    return out;
+}
+
+bool is_prefix(const std::string& s, const std::string& of) {
+    if (s.size() > of.size()) return false;
+
+    return std::equal(s.begin(), s.end(), of.begin());
+}
 
 class debugger {
     public:
@@ -12,10 +32,11 @@ class debugger {
         }
 
         void run();
-    
     private:
         std::string m_prog_name;
         pid_t m_pid;
+        void handle_command(const std::string& line);
+        void continue_execution();
 };
 
 void debugger::run() {
@@ -29,9 +50,31 @@ void debugger::run() {
 
     char* line = nullptr;
     while((line = linenoise("minidbg> ")) != nullptr) {
-
+        handle_command(line);
+        linenoiseHistoryAdd(line);
+        linenoiseFree(line);
     }
 
+}
+
+void debugger::handle_command(const std::string& line) {
+    auto args = split(line, ' ');
+    auto command = args[0];
+
+    if (is_prefix(command, "continue")) {
+        continue_execution();
+    }
+    else {
+        std::cerr << "Unknown command \n";
+    }
+}
+
+void debugger::continue_execution() {
+    ptrace(PTRACE_CONT, m_pid, nullptr, nullptr);
+
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
 }
 
 int main(int argc, char* argv[]) {
